accbcdf516b3a7628b2999769e0558621640bb39
aswinshan91 <130089402+aswinshan91@users.noreply.github.com>
2023-04-07 23:46:30 +0530

    Comments added in APIContext

--- networking/context/apiContext.jsx
+++ networking/context/apiContext.jsx
@@ -1,7 +1,9 @@
+/* implemented all the restful api call - GET, POST, PUT (users, post) */
 
 import React, { createContext, useState, useEffect } from "react";
 import https from '../https';
-import { Alert } from 'react-native';
+import { Alert } from 'react-native'
+import constant from '../../constants/constants';
 
 const APIContext = createContext();
 
@@ -10,70 +12,110 @@ export const APIProvider = ({ children }) => {
   const [userList, setUserList] = useState([]);
   const [userBlogList, setUserBlogList] = useState([]);
 
+/* Method - to call inital API for fetch user Details */
+
   useEffect(() => {
     fetchDashboard();
   }, []);
 
+  /* Method - to call user API for fetch user List */
+
   const fetchDashboardList = () => {
     https.get(`users/1/posts`)
       .then(response => {
+        if (response.data.length === 0) {
+          Alert.alert(JSON.stringify(constant.noData));
+          return;
+        }
         setUserList(response.data);
       })
       .catch(error => {
-        console.error('There was an error!', error);
+        if(error !== undefined || error !== {}){
+          Alert.alert(JSON.stringify(error.message));
+          return
+        }
+        Alert.alert(JSON.stringify(constant.serverdownError));
       });
-}
-
-    const fetchBlogDetails = () => {
-      https.get(`users/2/posts`)
-        .then(response => {
-          setUserBlogList(response.data);
-        })
-        .catch(error => {
-          console.error('There was an error!', error);
-        });
   }
 
-  const fetchDashboard = async () => {
+  /* Method - to call user API for fetch user List - used in Blog */
+
+  const fetchBlogDetails = () => {
+    https.get(`users/2/posts`)
+      .then(response => {
+        if (response.data.length === 0) {
+          Alert.alert(JSON.stringify(constant.noData));
+          return;
+        }
+        setUserBlogList(response.data);
+      })
+      .catch(error => {
+        if(error !== undefined || error !== {}){
+          Alert.alert(JSON.stringify(error.message));
+          return
+        }
+        Alert.alert(JSON.stringify(constant.serverdownError));
+      });
+  }
+
+  /* Method - to call user API for fetch user Details */
+
+  const fetchDashboard = () => {
     const userApiUrl = https.get(`users/1`)
       .then(response => {
+        if (response.data.length === 0) {
+          Alert.alert(JSON.stringify(constant.noData));
+          return;
+        }
         setUserDetails(response.data);
       })
       .catch(error => {
-        console.log(error);
+        if(error !== undefined || error !== {}){
+          Alert.alert(JSON.stringify(error.message));
+          return
+        }
+        Alert.alert(JSON.stringify(constant.serverdownError));
       });
-    // const userListApiUrl = https.get(`users/1/posts`)
-    //   .then(response => {
-    //     setUserList(response.data);
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
-
     Promise.all([userApiUrl]).then(function (response) {
       return response
     }).catch(function (err) {
-      console.log(err);
+      if(err !== undefined || err !== {}){
+        Alert.alert(JSON.stringify(err.message));
+        return
+      }
+      Alert.alert(JSON.stringify(constant.serverdownError));
     })
   };
 
-  const updateContentDetail = async(detail) => {
-    const article = {
+    /* Method - to call user post API */
+
+  const updateContentDetail = (detail) => {
+    const content = {
       body: detail.body,
       userId: detail.userId,
       id: detail.id,
       title: detail.title
     };
-    https.put(`posts/${detail.userId}`, article)
+    https.put(`posts/${detail.userId}`, content)
       .then(response => {
         if (response) {
+          if (response.data & response.data.length === 0) {
+            Alert.alert(JSON.stringify(constant.noData));
+            return;
+          }
           fetchDashboardList();
         }
-       })
+      })
       .catch(error => {
-        console.error('There was an error!', error);
+        if(error !== undefined || error !== {}){
+          Alert.alert(JSON.stringify(error.message));
+          return
+        }
+        Alert.alert(JSON.stringify(constant.serverdownError));
       });
   };
+
+  /* Added all the methods and properties for access globally */
 
   return (
     <APIContext.Provider
