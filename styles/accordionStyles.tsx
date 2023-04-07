ffd4f2e4eaf816018554ccbbb5bf016f4c9e74a2
aswinshan91 <130089402+aswinshan91@users.noreply.github.com>
2023-04-07 10:39:51 +0530

    styles update in accordion

--- styles/accordionStyles.tsx
+++ styles/accordionStyles.tsx
@@ -6,7 +6,11 @@ const accordionStyles = StyleSheet.create({
     },
     titleText: {
       fontSize: 12,
-    }
+    },
+    firstView: {
+      marginTop: 0, 
+      marginBottom: 10,
+    },
   })
 
   export default accordionStyles;
